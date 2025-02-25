3d845af7a509b0e5cee9e6ab4f58f2835af89abf
Harold <a050949359@gmail.com>
2025-02-25 08:13:17 +0000

    use config by viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,13 +6,11 @@ import (
 	"os"
 	"testing"
 
+	"github.com/golang_from_udemy/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:123456@127.0.0.1:5432/simple_bank?sslmode=disable"
-)
+
 
 var testQueries *Queries
 var testDB *sql.DB
@@ -20,7 +18,12 @@ var testDB *sql.DB
 func TestMain(m *testing.M) {
 	var err error
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,18 +6,24 @@ import (
 
 	"github.com/golang_from_udemy/api"
 	db "github.com/golang_from_udemy/db/sqlc"
+	"github.com/golang_from_udemy/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:123456@postgres:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgresql://root:123456@postgres:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+	
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -25,7 +31,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
